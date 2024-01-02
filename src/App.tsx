6c72e6f06c99c12ea63aaeb9f43e6ae5ebc19ffd
Atris <vacekj@outlook.com>
2024-01-02 21:50:28 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Button, Container, Heading } from "@chakra-ui/react";
+import {serialize, useContractWrite, useWalletClient} from "wagmi";
+import { usePermit } from "wagmi-permit";
+import {parseAbi, parseEther, zeroAddress} from "viem";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+function App() {
+	const { data: walletClient } = useWalletClient();
+	const { signPermit, signature, error } = usePermit({
+		walletClient,
+		ownerAddress: walletClient?.account.address ?? zeroAddress,
+		chainId: walletClient?.chain.id,
+		spenderAddress: "0x5f390415dB0f7d4D336095f3Fd266D6B3B616e7A", // vitalik.eth
+		contractAddress: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4", // usdc
+	});
+
+	const {write} = useContractWrite({
+		address: "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
+		abi:  parseAbi(["function permit(address owner,address spender,uint256 value,uint256 deadline,uint8 v,bytes32 r,bytes32 s)"]),
+		functionName: 'permit',
+		// @ts-ignore
+		args: ["0x5f390415dB0f7d4D336095f3Fd266D6B3B616e7A","0x5f390415dB0f7d4D336095f3Fd266D6B3B616e7A",10000000000000000000n,1704327012n,signature?.v, signature?.r,signature?.s]
+	});
+
+	return (
+		<Container my={3}>
+			<Heading as={"h1"} mb={5}>
+				Wagmi Permit Demo
+			</Heading>
+			<ConnectButton
+				chainStatus={{ smallScreen: "full", largeScreen: "full" }}
+				showBalance={{ smallScreen: true, largeScreen: true }}
+				accountStatus={{ smallScreen: "full", largeScreen: "full" }}
+			/>
+			{error && <pre>{serialize(error, null, 2)}</pre>}
+			{signature && <pre>{serialize(signature, null, 2)}</pre>}
+			{walletClient && (
+				<Button
+					mt={3}
+					onClick={async () => {
+						const permitSignature = await signPermit?.({
+							value: parseEther("10"),
+							deadline: 1704327012n,
+						});
+
+						console.log(permitSignature);
+					}}
+				>
+					Sign permit
+				</Button>
+			)}
+			<Button onClick={() => {
+				write?.();
+			}}>Permit</Button>
+		</Container>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { zkSync} from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+	[zkSync],
+	[publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+	appName: "Wagmi Permit Demo",
+	projectId: "YOUR_PROJECT_ID",
+	chains,
+});
+
+const wagmiConfig = createConfig({
+	autoConnect: true,
+	connectors,
+	publicClient,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<WagmiConfig config={wagmiConfig}>
+			<RainbowKitProvider chains={chains}>
+				<ChakraProvider>
+					<App />
+				</ChakraProvider>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	</React.StrictMode>,
+);
